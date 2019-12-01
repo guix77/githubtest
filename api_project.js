e982f70c9d633f6886f88fc87a9f671be0d8e453
guix77 <guix77@users.noreply.github.com>
2019-12-01 19:47:34 +0000

    Deploying to gh-pages from master 311203ce0ddcf5d73c34c5d77fad4922068e9d52

---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "githubtest",
+  "version": "1.0.0",
+  "description": "GitHub test API documentation",
+  "title": "GitHub test documentation",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-12-01T19:47:33.269Z",
+    "url": "http://apidocjs.com",
+    "version": "0.18.0"
+  }
+});
